d172306a6d71c8fb8a3b06ae52fb03ea75cbdda0
Aung Khant Kyaw <aungkhantkyaw8918.com>
2025-05-18 22:47:33 +0630

    Initial Commit

---
+++ SwiftfulThinkingContinueLearning/SwiftfulThinkingContinueLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingContinueLearningApp.swift
+//  SwiftfulThinkingContinueLearning
+//
+//  Created by Digital Base on 18/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingContinueLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
