2873b8e826c48a98f23d1bcb7ca9a0d9ba2faf65
Aung Khant Kyaw <aungkhantkyaw8918.com>
2025-05-22 16:53:22 +0630

    finished Download with Escaping

--- SwiftfulThinkingContinueLearning/SwiftfulThinkingContinueLearningApp.swift
+++ SwiftfulThinkingContinueLearning/SwiftfulThinkingContinueLearningApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftfulThinkingContinueLearningApp: App {
     var body: some Scene {
         WindowGroup {
-            BackgroundThreadBootcamp()
+            DownloadWithEscapingBootcamp()
         }
     }
 }
